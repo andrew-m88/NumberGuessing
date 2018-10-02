7ec7db8765e7bb084cc74bbc60aad5b3aa241fd9
Andrew Motley <andrew.motley.2021@student.lhschools.org>
2018-10-02 07:38:21 -0400

    added additional features to project in IntelliJ

--- src/NumberGuessing.java
+++ src/NumberGuessing.java
@@ -10,309 +10,43 @@ public class NumberGuessing {
         Scanner keyboard;
         int pickednumber;
         int guessednumber;
+        int maxattempt = 10;
+        int attempt = 1;
+        boolean correct;
 
         pickednumber = (int)(Math.random()*100);
         keyboard = new Scanner(System.in);
+        correct = false;
+
 
         System.out.println(pickednumber);
-        System.out.println("start guessing a number.");
-        guessednumber = keyboard.nextInt();
+        System.out.println("start guessing a number, you have 10 tries or you die.");
+        //guessednumber = keyboard.nextInt();
 
-        if (guessednumber<pickednumber){
-            System.out.println("your number is too low");
+        while(!correct&&attempt<=maxattempt) {
             System.out.println("you shall guess again");
             guessednumber = keyboard.nextInt();
-        }
-        else if (guessednumber>pickednumber){
-            System.out.println("your number is too high");
-            System.out.println("you shall guess again");
-            guessednumber = keyboard.nextInt();
-        }
-        if (guessednumber<pickednumber){
-            System.out.println("your number is too low");
-            System.out.println("you shall guess again");
-            guessednumber = keyboard.nextInt();
-        }
-        else if (guessednumber>pickednumber){
-            System.out.println("your number is too high");
-            System.out.println("you shall guess again");
-            guessednumber = keyboard.nextInt();
-        }
-        if (guessednumber<pickednumber){
-            System.out.println("your number is too low");
-            System.out.println("you shall guess again");
-            guessednumber = keyboard.nextInt();
-        }
-        else if (guessednumber>pickednumber){
-            System.out.println("your number is too high");
-            System.out.println("you shall guess again");
-            guessednumber = keyboard.nextInt();
-        }
-        if (guessednumber<pickednumber){
-            System.out.println("your number is too low");
-            System.out.println("you shall guess again");
-            guessednumber = keyboard.nextInt();
-        }
-        else if (guessednumber>pickednumber){
-            System.out.println("your number is too high");
-            System.out.println("you shall guess again");
-            guessednumber = keyboard.nextInt();
-        }
-        if (guessednumber<pickednumber){
-            System.out.println("your number is too low");
-            System.out.println("you shall guess again");
-            guessednumber = keyboard.nextInt();
-        }
-        else if (guessednumber>pickednumber){
-            System.out.println("your number is too high");
-            System.out.println("you shall guess again");
-            guessednumber = keyboard.nextInt();
-        }
-        if (guessednumber<pickednumber){
-            System.out.println("your number is too low");
-            System.out.println("you shall guess again");
-            guessednumber = keyboard.nextInt();
-        }
-        else if (guessednumber>pickednumber){
-            System.out.println("your number is too high");
-            System.out.println("you shall guess again");
-            guessednumber = keyboard.nextInt();
-        }
-        if (guessednumber<pickednumber){
-            System.out.println("your number is too low");
-            System.out.println("you shall guess again");
-            guessednumber = keyboard.nextInt();
-        }
-        else if (guessednumber>pickednumber){
-            System.out.println("your number is too high");
-            System.out.println("you shall guess again");
-            guessednumber = keyboard.nextInt();
-        }
-        if (guessednumber<pickednumber){
-            System.out.println("your number is too low");
-            System.out.println("you shall guess again");
-            guessednumber = keyboard.nextInt();
-        }
-        else if (guessednumber>pickednumber){
-            System.out.println("your number is too high");
-            System.out.println("you shall guess again");
-            guessednumber = keyboard.nextInt();
-        }
-        if (guessednumber<pickednumber){
-            System.out.println("your number is too low");
-            System.out.println("you shall guess again");
-            guessednumber = keyboard.nextInt();
-        }
-        else if (guessednumber>pickednumber){
-            System.out.println("your number is too high");
-            System.out.println("you shall guess again");
-            guessednumber = keyboard.nextInt();
-        }
-        if (guessednumber<pickednumber){
-            System.out.println("your number is too low");
-            System.out.println("you shall guess again");
-            guessednumber = keyboard.nextInt();
-        }
-        else if (guessednumber>pickednumber){
-            System.out.println("your number is too high");
-            System.out.println("you shall guess again");
-            guessednumber = keyboard.nextInt();
-        }
-        if (guessednumber<pickednumber){
-            System.out.println("your number is too low");
-            System.out.println("you shall guess again");
-            guessednumber = keyboard.nextInt();
-        }
-        else if (guessednumber>pickednumber){
-            System.out.println("your number is too high");
-            System.out.println("you shall guess again");
-            guessednumber = keyboard.nextInt();
-        }
-        if (guessednumber<pickednumber){
-            System.out.println("your number is too low");
-            System.out.println("you shall guess again");
-            guessednumber = keyboard.nextInt();
-        }
-        else if (guessednumber>pickednumber){
-            System.out.println("your number is too high");
-            System.out.println("you shall guess again");
-            guessednumber = keyboard.nextInt();
-        }
-        if (guessednumber<pickednumber){
-            System.out.println("your number is too low");
-            System.out.println("you shall guess again");
-            guessednumber = keyboard.nextInt();
-        }
-        else if (guessednumber>pickednumber){
-            System.out.println("your number is too high");
-            System.out.println("you shall guess again");
-            guessednumber = keyboard.nextInt();
-        }
-        if (guessednumber<pickednumber){
-            System.out.println("your number is too low");
-            System.out.println("you shall guess again");
-            guessednumber = keyboard.nextInt();
-        }
-        else if (guessednumber>pickednumber){
-            System.out.println("your number is too high");
-            System.out.println("you shall guess again");
-            guessednumber = keyboard.nextInt();
-        }
-        if (guessednumber<pickednumber){
-            System.out.println("your number is too low");
-            System.out.println("you shall guess again");
-            guessednumber = keyboard.nextInt();
-        }
-        else if (guessednumber>pickednumber){
-            System.out.println("your number is too high");
-            System.out.println("you shall guess again");
-            guessednumber = keyboard.nextInt();
-        }
-        if (guessednumber<pickednumber){
-            System.out.println("your number is too low");
-            System.out.println("you shall guess again");
-            guessednumber = keyboard.nextInt();
-        }
-        else if (guessednumber>pickednumber){
-            System.out.println("your number is too high");
-            System.out.println("you shall guess again");
-            guessednumber = keyboard.nextInt();
-        }
-        if (guessednumber<pickednumber){
-            System.out.println("your number is too low");
-            System.out.println("you shall guess again");
-            guessednumber = keyboard.nextInt();
-        }
-        else if (guessednumber>pickednumber){
-            System.out.println("your number is too high");
-            System.out.println("you shall guess again");
-            guessednumber = keyboard.nextInt();
-        }
-        if (guessednumber<pickednumber){
-            System.out.println("your number is too low");
-            System.out.println("you shall guess again");
-            guessednumber = keyboard.nextInt();
-        }
-        else if (guessednumber>pickednumber){
-            System.out.println("your number is too high");
-            System.out.println("you shall guess again");
-            guessednumber = keyboard.nextInt();
-        }
-        if (guessednumber<pickednumber){
-            System.out.println("your number is too low");
-            System.out.println("you shall guess again");
-            guessednumber = keyboard.nextInt();
-        }
-        else if (guessednumber>pickednumber){
-            System.out.println("your number is too high");
-            System.out.println("you shall guess again");
-            guessednumber = keyboard.nextInt();
-        }
-        if (guessednumber<pickednumber){
-            System.out.println("your number is too low");
-            System.out.println("you shall guess again");
-            guessednumber = keyboard.nextInt();
-        }
-        else if (guessednumber>pickednumber){
-            System.out.println("your number is too high");
-            System.out.println("you shall guess again");
-            guessednumber = keyboard.nextInt();
-        }
-        if (guessednumber<pickednumber){
-            System.out.println("your number is too low");
-            System.out.println("you shall guess again");
-            guessednumber = keyboard.nextInt();
-        }
-        else if (guessednumber>pickednumber){
-            System.out.println("your number is too high");
-            System.out.println("you shall guess again");
-            guessednumber = keyboard.nextInt();
-        }
-        if (guessednumber<pickednumber){
-            System.out.println("your number is too low");
-            System.out.println("you shall guess again");
-            guessednumber = keyboard.nextInt();
-        }
-        else if (guessednumber>pickednumber){
-            System.out.println("your number is too high");
-            System.out.println("you shall guess again");
-            guessednumber = keyboard.nextInt();
-        }
-        if (guessednumber<pickednumber){
-            System.out.println("your number is too low");
-            System.out.println("you shall guess again");
-            guessednumber = keyboard.nextInt();
-        }
-        else if (guessednumber>pickednumber){
-            System.out.println("your number is too high");
-            System.out.println("you shall guess again");
-            guessednumber = keyboard.nextInt();
-        }
-        if (guessednumber<pickednumber){
-            System.out.println("your number is too low");
-            System.out.println("you shall guess again");
-            guessednumber = keyboard.nextInt();
-        }
-        else if (guessednumber>pickednumber){
-            System.out.println("your number is too high");
-            System.out.println("you shall guess again");
-            guessednumber = keyboard.nextInt();
-        }
-        if (guessednumber<pickednumber){
-            System.out.println("your number is too low");
-            System.out.println("you shall guess again");
-            guessednumber = keyboard.nextInt();
-        }
-        else if (guessednumber>pickednumber){
-            System.out.println("your number is too high");
-            System.out.println("you shall guess again");
-            guessednumber = keyboard.nextInt();
-        }
-        if (guessednumber<pickednumber){
-            System.out.println("your number is too low");
-            System.out.println("you shall guess again");
-            guessednumber = keyboard.nextInt();
-        }
-        else if (guessednumber>pickednumber){
-            System.out.println("your number is too high");
-            System.out.println("you shall guess again");
-            guessednumber = keyboard.nextInt();
-        }
-        if (guessednumber<pickednumber){
-            System.out.println("your number is too low");
-            System.out.println("you shall guess again");
-            guessednumber = keyboard.nextInt();
-        }
-        else if (guessednumber>pickednumber){
-            System.out.println("your number is too high");
-            System.out.println("you shall guess again");
-            guessednumber = keyboard.nextInt();
-        }
-        if (guessednumber<pickednumber){
-            System.out.println("your number is too low");
-            System.out.println("you shall guess again");
-            guessednumber = keyboard.nextInt();
-        }
-        else if (guessednumber>pickednumber){
-            System.out.println("your number is too high");
-            System.out.println("you shall guess again");
-            guessednumber = keyboard.nextInt();
-        }
-        if (guessednumber<pickednumber){
-            System.out.println("your number is too low");
-            System.out.println("you shall guess again");
-            guessednumber = keyboard.nextInt();
-        }
-        else if (guessednumber>pickednumber){
-            System.out.println("your number is too high");
-            System.out.println("you shall guess again");
-            guessednumber = keyboard.nextInt();
-        }
-        else if (guessednumber==pickednumber){
-            System.out.println("you are correct");
-            System.out.println("GAME OVER");
+            attempt++;
+            if (guessednumber < pickednumber) {
+                System.out.println("your number is too low");
+
+            } else if (guessednumber > pickednumber) {
+                System.out.println("your number is too high");
+
+            } else if (guessednumber == pickednumber) {
+                System.out.println("you are correct");
+                System.out.println("GAME OVER");
+
+                correct = true;
+                break;
+            }
+
+
+
+
         }
 
+        System.out.println("outside of while loop");
 
 
 
